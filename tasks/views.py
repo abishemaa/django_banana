4b513f2c9c8c6f28eb2fcc4b4668f7a4317cca51
a267 <abishemaj2@gmail.com>
2025-05-22 09:09:34 +0530

    som updates on "tasks" app

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
+from django import forms
+
+
+tasks = ["Task 1", "Task 2", "Task 3"]
 
 # Create your views here.
 def index(request):
     # Render the index.html template
-    return render(request, "tasks/index.html")
+    return render(request, "tasks/index.html", {
+        "tasks": tasks
+    })
+
+def add(request):
+    return render(request, "tasks/add.html")
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+
+# Add a new task:
+def add(request):
+    return render(request, "tasks/add.html", {
+        "form": NewTaskForm()
+    })
